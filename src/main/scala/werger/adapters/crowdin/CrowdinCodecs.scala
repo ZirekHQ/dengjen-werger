f4f3110e9b86088e62f52cdfbef69d1c0c726dbb
Ali Ustek <13117393+austek@users.noreply.github.com>
2026-09-12 17:17:27 +0100

    feat: add TranslationSource port and Crowdin JSON codecs (#39)

---
+++ src/main/scala/werger/adapters/crowdin/CrowdinCodecs.scala
@@ -0,0 +1,16 @@
+package werger.adapters.crowdin
+
+import io.circe.Decoder
+import io.circe.generic.semiauto.*
+
+final case class CrowdinSourceString(id: Long, text: String, identifier: String)
+object CrowdinSourceString:
+  given Decoder[CrowdinSourceString] = deriveDecoder
+
+final case class CrowdinTranslation(id: Long, stringId: Long, text: String)
+object CrowdinTranslation:
+  given Decoder[CrowdinTranslation] = deriveDecoder
+
+final case class CrowdinCreatedTranslation(id: Long)
+object CrowdinCreatedTranslation:
+  given Decoder[CrowdinCreatedTranslation] = deriveDecoder
